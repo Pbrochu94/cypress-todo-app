9f92395fc0662f57899f367b645278ca5e4d97b9
Patrick Brochu <patrickbrochu70@gmail.com>
2025-06-12 17:30:53 -0400

    chore: implement test case that check if new todo is added to the todo list

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,5 +1,12 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("add todo functionalities", () => {
+  beforeEach(() => {
     cy.visit("/index.html");
+  });
+  it("add new todo to list", () => {
+    cy.get(".new-todo")
+      .should("have.attr", "placeholder", "What needs to be done?")
+      .click()
+      .type("Workout{enter}");
+    cy.get(".todo-list").should("have.length", "1");
   });
 });
