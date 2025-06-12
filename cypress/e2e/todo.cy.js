631c98a0926200ba755f01760e0d15d3694b4092
Patrick Brochu <patrickbrochu70@gmail.com>
2025-06-12 18:37:53 -0400

    chore:finished the add todo test suite feat:started the ui testing suite
    TODO: keep working on the ui etsting suite

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,12 +1,49 @@
+//utilities
+function addTask(task) {
+  cy.get("@todoBar")
+    .should("have.attr", "placeholder", "What needs to be done?")
+    .click()
+    .type(`${task}{enter}`);
+}
+let tasks = [
+  "Workout",
+  "Web dev",
+  "Laundry",
+  "Stream",
+  "Take a walk",
+  "Invest",
+  "Take a 15 min relaxation break",
+];
+
+//tests
 describe("add todo functionalities", () => {
   beforeEach(() => {
     cy.visit("/index.html");
+    cy.get(`.todo-list`).as(`list`); //define the list selector as list
+    cy.get(`.new-todo`).as(`todoBar`); //define the list selector as list
   });
-  it("add new todo to list", () => {
-    cy.get(".new-todo")
-      .should("have.attr", "placeholder", "What needs to be done?")
-      .click()
-      .type("Workout{enter}");
-    cy.get(".todo-list").should("have.length", "1");
+  it("add new todos to list", () => {
+    tasks.forEach((task) => {
+      //check for each tasks in the array
+      addTask(task);
+      cy.get(`@list`).children().first().should(`contain.text`, `${task}`);
+    });
+    cy.get(".todo-list").children().should("have.length", tasks.length);
   });
+  it("user cannot add an empty string", () => {
+    cy.get(`@todoBar`).click().type("{enter}");
+    cy.get(`@list`).children().should(`have.length`, 0);
+    cy.get(`@todoBar`).should(`have.value`, ``);
+  });
+});
+describe("add todo UI validation", () => {
+  beforeEach(() => {
+    cy.visit("/index.html");
+    cy.get(`.todo-list`).as(`list`); //define the list selector as list
+    cy.get(`.new-todo`).as(`todoBar`); //define the list selector as list
+    tasks.forEach((task) => {
+      addTask(task);
+    });
+  });
+  it("check circle successfully appears next to the new todo", () => {});
 });
